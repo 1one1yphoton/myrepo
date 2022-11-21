216520a69dabf68ef6dff0c9c672046ebeff71be
1one1yphoton <1one1yphoton@proton.me>
2022-11-20 22:55:22 -0800

    added detail to explanations

--- PAT.R
+++ PAT.R
@@ -1,9 +1,9 @@
 # generate a personal access token (PAT)
 usethis::create_github_token()
 
-# in git, select "repo", "user", and "workflow". Change expiration or accept default 30d
+# in git, select "repo", "user", and "workflow". Change expiration or accept default 30d for security 
 
 # copy and save PAT in password software, as well as in R
 gitcreds::gitcreds_set()
 
-# paste PAT
+# paste PAT. GitHub should allow push and pull now
